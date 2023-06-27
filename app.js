c73a5b8e868c2e135067dcb321fa02dc1e9e099a
TKYOpartner <128873901+TKYOpartner@users.noreply.github.com>
2023-06-27 12:31:17 -0400

    Added more standard colors

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const colors = ["green", "red", "rgba(133,122,200)", "f15025"];
+const colors = ["Green", "Red", "Yellow", "Purple", "Brown", "Pink", "Grey", "Cyan", "Coral", "Violet", "Olive"];
 const btn = document.getElementById("btn");
 const color = document.querySelector(".color");
 
